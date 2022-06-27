01e4df39a595a756f5bb589610012b55a4eaf417
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2022-06-27 12:25:48 +0530

    Update dashboard.js

--- screens/dashboard.js
+++ screens/dashboard.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "../navigation/tabnavigator";
-
+import StackNavigator from "../navigation/stacknavigator";
 export default function DashboardScreen() {
     return (
         <NavigationContainer>
-            <BottomTabNavigator />
+            <StackNavigator />
         </NavigationContainer>
     );
 }
